f18142bcd7b5953251689ac8ccd4c813d642214c
BT_C09_15 <mobileappdev@bths.edu>
2026-04-28 09:18:24 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
