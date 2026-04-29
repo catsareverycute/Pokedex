c8a9fd84c1f471a181bb812f68ef9471b88b8097
BT_C09_04 <mobileappdev@bths.edu>
2026-04-29 08:56:32 -0400

    PokeInfo file created

---
+++ Pokedex/PokeInfo.swift
@@ -0,0 +1,26 @@
+//
+//  PokeInfo.swift
+//  Pokedex
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+struct PokeInfo: View {
+    var body: some View {
+        VStack {
+            Text("The following are placeholders:\n")
+            Text("Name")
+            Text("Image")
+            Text("Type")
+            Text("Evolutions")
+            Text("Description")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PokeInfo()
+}
