86b8522a5273c5ecf2074775d0bed880b84ccbd6
BT_C09_15 <mobileappdev@bths.edu>
2026-04-30 09:29:02 -0400

    added random pokemon image function

--- Pokedex/NetworkClient.swift
+++ Pokedex/NetworkClient.swift
@@ -5,3 +5,20 @@
 //  Created by Student on 4/30/26.
 //
 
+import SwiftUI
+import Foundation
+
+@Observable
+class NetworkClient {
+    private(set) var pokemonImageURL: URL? = nil
+    
+    private let totalPokemon = 151
+    
+    func getRandomPokemon() {
+        let randomID = Int.random(in: 1...totalPokemon)
+
+        let urlString = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(randomID).png"
+   
+        self.pokemonImageURL = URL(string: urlString)
+    }
+}
