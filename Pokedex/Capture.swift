702598296a0fa8e37f9087a10613e6fc738e1675
BT_C09_04 <mobileappdev@bths.edu>
2026-05-06 08:58:38 -0400

    Animation

--- Pokedex/Capture.swift
+++ Pokedex/Capture.swift
@@ -3,9 +3,13 @@ import SwiftUI
 struct Capture: View {
     @State private var client = NetworkClient()
     @State private var animate = false
+    @State private var buttonVisible = true
+    @State private var ballLoc = CGSize(width: 0, height: 575)
     
     var body: some View {
         ZStack {
+            Image(.battlePlatform)
+                .offset(x: 0, y: 40)
             if let pokemon = client.currentPokemon {
                 AsyncImage(url: URL(string: pokemon.sprites.front_default)) { image in
                     image.resizable().scaledToFit()
@@ -15,21 +19,44 @@ struct Capture: View {
                 .frame(width: 300, height: 300)
                 .onAppear { resetAndStart() }
                 .opacity(animate ? 0 : 1)
-                .offset(x: Constants.pokemonLoc.x, y: Constants.pokemonLoc.y)
-//                .offset(x: animate ? CGFloat.random(in: -100...100) : 0)
-//                .offset(y: animate ? CGFloat.random(in: -100...100) : 0)
+                .offset(x: 0, y: 0)
+                //                .offset(x: animate ? CGFloat.random(in: -100...100) : 0)
+                //                .offset(y: animate ? CGFloat.random(in: -100...100) : 0)
                 .onAppear { resetAndStart() }
                 .onChange(of: client.animationTrigger) { resetAndStart() }
             }
             VStack {
                 Image(.pokeball)
-                    .offset(x: Constants.ballLoc.x, y: Constants.ballLoc.y)
+                    .offset(ballLoc)
+                    .gesture(DragGesture(minimumDistance: 30, coordinateSpace: .local)
+                        .onEnded({ value in
+//                            if value.translation.height < 10 {
+//                                withAnimation(
+//                                    ballLoc = CGPoint(0, 0)
+//                                )
+//                            }
+                            withAnimation(.easeInOut(duration: 0.5)){
+                                ballLoc = CGSize(width: 0, height: 100)
+                            }
+                            withAnimation(.easeInOut(duration: 0.4).delay(0.5)){
+                                ballLoc = CGSize(width: 0, height: 260)
+                            }
+                            withAnimation(.easeInOut(duration: 0.3).delay(0.9)){
+                                ballLoc = CGSize(width: 0, height: 150)
+                            }
+                            withAnimation(.easeInOut(duration: 0.3).delay(1.2)){
+                                ballLoc = CGSize(width: 0, height: 260)
+                            }
+                        }))
                 Spacer()
                 Button("Launch Pokemon") {
                     Task {
                         await client.getRandomPokemon()
+                        ballLoc = CGSize(width: 0, height: 575)
+                        buttonVisible = false
                     }
                 }
+                .disabled(!buttonVisible)
                 .onAppear {
                     Task {
                         await client.getRandomPokemon()
@@ -40,6 +67,7 @@ struct Capture: View {
             .padding(.bottom, 50)
         }
     }
+    
     func resetAndStart() {
         animate = false
         withAnimation(.linear(duration: 3.0)) {
